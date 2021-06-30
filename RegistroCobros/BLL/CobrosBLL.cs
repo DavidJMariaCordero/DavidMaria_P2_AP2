e0e1a6c8bcb990d7afd9dd8b30784e3fde9ca514
David Maria <eldavid_46@hotmail.com>
2021-06-30 19:29:00 -0400

    Cambios en Cobros BLL

--- RegistroCobros/BLL/CobrosBLL.cs
+++ RegistroCobros/BLL/CobrosBLL.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroCobros.DAL;
+using RegistroCobros.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +10,98 @@ namespace RegistroCobros.BLL
 {
     public class CobrosBLL
     {
+        public static bool Guardar(Cobros cobro)
+        {
+            if (!Existe(cobro.CobroId))
+                return Insertar(cobro);
+            else
+                return false;
+        }
+
+        private static bool Insertar(Cobros cobro)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+            try
+            {
+                contexto.Cobros.Add(cobro);
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+        public static bool Eliminar(int id)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+            try
+            {
+                var cobro = CobrosBLL.Buscar(id);
+
+                if (cobro != null)
+                {
+                    contexto.Cobros.Remove(cobro);
+                    paso = contexto.SaveChanges() > 0;
+                }
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+        public static Cobros Buscar(int id)
+        {
+            Cobros cobro = new Cobros();
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                cobro = contexto.Cobros.Include(c => c.Detalle)
+                    .Where(c => c.CobroId == id)
+                    .SingleOrDefault();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return cobro;
+        }
+        public static bool Existe(int id)
+        {
+            Contexto contexto = new Contexto();
+            bool encontrado = false;
+
+            try
+            {
+                encontrado = contexto.Cobros.Any(c => c.CobroId == id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return encontrado;
+        }
     }
 }
